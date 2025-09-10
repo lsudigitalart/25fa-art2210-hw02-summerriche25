151bd05857d0cd45cc2e1ea3c984794c701a8195
Summer Riche <sriche9@lsu.edu>
2025-09-10 14:41:53 -0500

    roughdraftimage16new

--- sketch.js
+++ sketch.js
@@ -4,16 +4,39 @@ function setup() {
 
 function draw() {
 
-  background  (220);
   // halftone dots ellipses;
-
+  background  (220);
+  
   strokeWeight (7);
   stroke ("white");
-  line (1, 75, 200, 75);
+  line (1, 75, 190, 75);
 
   strokeWeight (12);
   stroke ("black");
-  line (1, 120, 210, 120);
+  line (1, 120, 200, 120);
 
+  strokeWeight (5);
+  stroke ("black");
+  line (1, 50, 180, 50);
+
+  strokeWeight (5);
+  stroke ("black");
+  line(150, 1, 300, 400);
+
+  strokeWeight (40);
+  stroke ("white");
+  line(174, 1, 325, 400);
+
+  strokeWeight (5);
+  stroke ("black");
+  line(194, 1, 350, 400);
+
+  strokeWeight (25);
+  stroke ("black");
+  line(350, 1, 400, 400);
+
+  strokeWeight (20);
+  stroke ("black");
+  line(270, -10, 400, 76);
 
 }
